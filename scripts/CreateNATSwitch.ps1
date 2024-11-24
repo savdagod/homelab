2dd6e560138a1a6576e02c35a18fee1581ed902d
Bruce <107087485+savdagod@users.noreply.github.com>
2024-11-24 13:57:45 -0500

    Add useful scripts

---
+++ scripts/CreateNATSwitch.ps1
@@ -0,0 +1,7 @@
+﻿$SwitchName = "Default Switch"
+New-VMSwitch -Name $SwitchName -SwitchType Internal
+
+$if = Get-NetAdapter | Where-Object {$_.Name -eq $SwitchName} | Select-Object -ExpandProperty ifIndex
+New-NetNat -name InternalNATNetwork -InternalIPInterfaceAddressPrefix 172.16.0.0/24
+New-NetIPAddress -IPAddress 172.16.0.1 -PrefixLength 24 -InterfaceIndex $if
+New-NetFirewallRule -DisplayName "Allow NAT" -Direction Inbound -Protocol Any -LocalAddress Any -RemoteAddress 172.16.0.0/24 -Action Allow
